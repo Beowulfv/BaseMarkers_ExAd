1415f445e2b18a82641f643562f991415198e5a3
Beowulfv <beowulfv@gmail.com>
2017-02-08 15:00:20 -0500

    Add files via upload

---
+++ BaseMarker_ExAd/config.cpp
@@ -0,0 +1,12 @@
+class CfgXM8
+{
+	extraApps[] = {"BaseMarker"};
+	
+	class BaseMarker 
+	{
+		controlID = 50500;
+		title = "Base Marker";		
+		logo = "ExAdClient\XM8\Apps\BaseMarker\BaseMarker.paa";
+		onLoad = "ExAdClient\XM8\Apps\BaseMarker\onLoad.sqf";
+	};
+}; 
